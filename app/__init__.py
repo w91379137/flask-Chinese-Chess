da09e492dec395c2eb5d8301df6595a7bce743e8
w91379137 <w913791379137@gmail.com>
2021-08-04 05:43:51 +0800

    新增 board

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,15 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app(test_config = None):
     app = Flask(__name__)
+
+    
+    # 首頁設定
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        return render_template('board.html')
+
 
     return app
