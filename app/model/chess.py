745fc1b389bdc845c789dd304f536b5e19e60e20
w91379137 <w913791379137@gmail.com>
2021-08-05 19:21:55 +0800

    新增走路限制

--- app/model/chess.py
+++ app/model/chess.py
@@ -33,8 +33,29 @@ class Chess():
                 self.take = index
         else:
             if self.board[index] == space:
-                self.board[index] = self.board[self.take]
-                self.board[self.take] = space
-                self.take = None
+
+                if self.is_next(index, self.take):
+                    self.board[index] = self.board[self.take]
+                    self.board[self.take] = space
+                    self.take = None
+                else:
+                    self.error = f'不能下在 {index}'
             else:
                 self.error = f'有東西 {index}'
+
+    def is_next(self, a, b):
+        x_a, y_a = self.map(a)
+        x_b, y_b = self.map(b)
+
+        if (x_a == x_b):
+            return abs(y_a - y_b) == 1
+
+        if (y_a == y_b):
+            return abs(x_a - x_b) == 1
+        
+        return False
+
+    def map(self, index):
+        x = index % 8
+        y = (index - x) / 8
+        return x, y
